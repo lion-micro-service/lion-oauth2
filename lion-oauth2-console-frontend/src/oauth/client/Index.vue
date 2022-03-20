1d2b6147ab8901a8bab22e22f3ad2fe23101ea42
mr.liu <158442534@qq.com>
2022-03-20 23:41:50 +0800

    优化

--- lion-oauth2-console-frontend/src/oauth/client/Index.vue
+++ lion-oauth2-console-frontend/src/oauth/client/Index.vue
@@ -1,9 +1,9 @@
 <template>
   <router-view v-slot="{ Component }">
     <keep-alive>
-      <component :is="Component"  v-if="$route.meta.keepAlive"/>
+      <component :is="Component" :key="$route.name"   v-if="$route.meta.keepAlive"/>
     </keep-alive>
-    <component :is="Component"  v-if="!$route.meta.keepAlive"/>
+    <component :is="Component" :key="$route.name"   v-if="!$route.meta.keepAlive"/>
   </router-view>
 </template>
 
